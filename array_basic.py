1d761e46ea80bc5b7cc3c2f18da24bb299591e86
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-03-04 01:05:15 +0530

    update

--- array_basic.py
+++ array_basic.py
@@ -1,1 +1,16 @@
 # Removing Duplicate
+def removing_dup(arr):
+    final = set()
+
+    for i in range(len(arr)):
+        if arr[i] not in final:
+            final.add(i)
+        if 0 in final:
+            final.remove(0)
+
+    fi = list(final)
+
+    return fi
+
+arr = [1,2,3,4,1,4]
+print(removing_dup(arr))
