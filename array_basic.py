e321b96e0821a1d0afcb317b34dd3f4d16065787
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-03-04 12:09:33 +0530

    update

--- array_basic.py
+++ array_basic.py
@@ -2,9 +2,9 @@
 def removing_dup(arr):
     final = set()
 
-    for i in range(len(arr)):
-        if arr[i] not in final:
-            final.add(arr[i])
+    for i in arr:
+        if i not in final:
+            final.add(i)
 
     return list(set(arr))
 
--- array_basic5.py
+++ array_basic5.py
@@ -1,12 +1,13 @@
 # Counting the occurances
 def check_occ(arr,check_num):
     int_arr = list(map(int,arr))
-    count = 1
-    for i in range(len(int_arr)):
-        if check_num in int_arr:
+    check_num = int(check_num)
+    count = 0
+    for i in int_arr:
+        if i ==  check_num:
             count += 1
-            return count
-        
+    return count
+    
 arr = ["1","2","1","4"]
 check_num = 1
 print(check_occ(arr,check_num))
