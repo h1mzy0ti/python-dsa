ae2c6c134358dd9d24b81c370bda24f79d3afd57
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-03-18 20:04:42 +0530

    update

---
+++ selection_sort.py
@@ -0,0 +1,12 @@
+arr = [12,45,1.-23,45]
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        minval = i
+        for j in range(i+1,n):
+            if arr[j] < arr[minval]:
+                minval = j
+        arr[i],arr[minval] = arr[minval],arr[i]
+    return arr
+
+print(selection_sort(arr))
