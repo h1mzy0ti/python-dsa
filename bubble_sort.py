c2498f276e27292d346f071667b221b3f32f1be2
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-03-18 19:55:55 +0530

    update

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,8 @@
 arr = [2,3,1,3,4-12]
 
 def bubble_sort(arr):
-    n = len(arr) - 1
-    for i in range(n):
+    n = len(arr)
+    for i in range(n - 1):
         for j in range(n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j],arr[j+1] = arr[j+1],arr[j]
