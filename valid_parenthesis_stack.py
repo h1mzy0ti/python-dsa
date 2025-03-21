ae784db2e992206dd46a809608a703497b971438
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-03-21 12:26:08 +0530

    update

---
+++ valid_parenthesis_stack.py
@@ -0,0 +1,14 @@
+s = "([{}])"
+def solve(s):
+    stack = []
+    closeToOpen = {")":"(","}":"{","]":"[" }
+    for char in s:
+        if char in closeToOpen:
+            if stack and stack[-1] == closeToOpen[char]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(char)
+    return True if not stack else False
+print(solve(s))
