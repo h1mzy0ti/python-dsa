fb4033aa274721ece42997e09eeb47f7db03a386
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-04-11 20:53:54 +0530

    update

--- Array/array_basics.py
+++ Array/array_basics.py
@@ -1,10 +1,11 @@
 # List multiplication in Python language Result Array Preprocessing or simply Result Initialization
 
 arr = [2,4,52,23]
-tem = [-1] * 2
+tem0 = [-1] * 4
 
 tem1 = [0] * len(arr)
 
 tem1[0] = 23
+tem1[2] = 9
 
-print(tem,tem1,sep="\n") 
+print(tem0,tem1,sep="\n") 
