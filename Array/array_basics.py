040354c74535a1efae9cf5eec1cc18c8fe63e6d0
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-04-11 20:39:14 +0530

    added result array reprocessing

---
+++ Array/array_basics.py
@@ -0,0 +1,10 @@
+# List multiplication in Python language Result Array Preprocessing or simply Result Initialization
+
+arr = [2,4,52,23]
+tem = [-1] * 2
+
+tem1 = [0] * len(arr)
+
+tem1[0] = 23
+
+print(tem,tem1,sep="\n") 
