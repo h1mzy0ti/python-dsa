90eb7fffc5f5fd9b30575bdd1bb0df58411d5c74
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-04-14 22:57:05 +0530

    update

--- Monotonic Stack/m_stack_prac1.py
+++ Monotonic Stack/m_stack_prac1.py
@@ -1,11 +1,22 @@
+# Monotonic Stack (Next Greater Element) same array
+
 arr = [2, 1, 3]
+
 stack = []
 result = [-1] * len(arr)
 
-for nums in range(len(arr)):
-    while stack and arr[nums] > arr[stack[-1]]:
+for n in range(len(arr)):
+    while stack and arr[n] > arr[stack[-1]]:
         index = stack.pop()
-        result[index] = arr[nums]
-    stack.append(nums)
+        result[index] = arr[n]
+    stack.append(n)
 
-print(result)
+
+
+
+
+
+
+
+
+
