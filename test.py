7994adc295deddd7174d1cd41eafa92a10010d4c
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-04-14 22:57:25 +0530

    update

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
-nums = [23,2,14,12]
-def isMonotonic(nums):
+class car:
+    def __init__(self,model,year,manufacturer):
+        self.year = year
+        self.__model = model
+        self.manufacturer = manufacturer
 
-    for n in range(len(nums)-1):
-        if nums[n] > nums[n+1]:
-            return False
-        elif nums[n] < nums[n+1]:
-            return False
-    return True
-print(isMonotonic(nums))
+    def getmodel(self):
+        return self.__model
+
+class suv(car):
+    def specification(self):
+        self.specs = "20"
+        return self.specs
+
+
+car = car("BD",2025,"BYD")
+
+
+print(car.getmodel())
+
+car2 = suv(2023,"BH","Hammar")
+
+print(car2.specification())
