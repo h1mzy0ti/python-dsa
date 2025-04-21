5e0b1aa8a6e37e6441c2fe589e0a9f2a0b3fd0cc
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-04-21 19:34:06 +0530

    added array(roman_to_int)

---
+++ Array/roman_to_int.py
@@ -0,0 +1,39 @@
+'''Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+'''
+s = "IV"
+
+def roman_to_int(arr):
+    value_map = {
+        "I": 1, 
+        "V": 5, "X": 10,
+        "L": 50, "C": 100,
+        "D": 500, "M": 1000
+    }
+    total = 0
+    previous  = 0
+
+    for v in reversed(s):
+        current = value_map[v]
+        if current < previous:
+            total -= current
+        else:
+            total += current
+        previous = current
+    return total
+print(roman_to_int(s))
+            
--- test.py
+++ test.py
@@ -1,23 +1,2 @@
-class car:
-    def __init__(self,model,year,manufacturer):
-        self.year = year
-        self.__model = model
-        self.manufacturer = manufacturer
-
-    def getmodel(self):
-        return self.__model
-
-class suv(car):
-    def specification(self):
-        self.specs = "20"
-        return self.specs
-
-
-car = car("BD",2025,"BYD")
-
-
-print(car.getmodel())
-
-car2 = suv(2023,"BH","Hammar")
-
-print(car2.specification())
+arr = [-1,-89]
+print(min(arr))
