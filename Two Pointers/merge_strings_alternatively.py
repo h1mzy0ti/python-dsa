f3658b483517af8cce99a1d5db551d2d279b8193
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-04-21 20:47:18 +0530

    merge_strings_alternately

--- Two Pointers/merge_strings_alternatively.py
+++ Two Pointers/merge_strings_alternatively.py
@@ -1,17 +1,18 @@
 word1 = "abc"
 word2 = "pqrt"
 # Expected oputput = apbqcr
-def mergeAlternately(word1: str, word2: str) -> str:
-    w1l,w2l = len(word1), len(word2)
-    res = []
 
-    for i in range(max(w1l,w2l)):
-        if i < w1l:
+def mergeAlternately(word1: str, word2: str) -> str:
+    w1l,w2l = len(word1), len(word2)                # for lenght
+    res = []                                        # To store the each letter after one iteration
+
+    for i in range(max(w1l,w2l)):                   # Contdition to  run untill the max lenght
+        if i < w1l:                                 # if i is less than the lenght of word1 the letter will be added otherwise pass
             res.append(word1[i])
-        if i < w2l:
+        if i < w2l:                                 # if i is less than the lenght of word2 the letter will be added otherwise pass
             res.append(word2[i])
 
-    return ''.join(res)
+    return ''.join(res)                             # Trick to convert the list into string where ''.join(res)
 
 print(mergeAlternately(word1,word2))
 
