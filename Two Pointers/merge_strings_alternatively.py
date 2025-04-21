f608b045e52b58b030b8e0cddec9df5da3c79c63
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-04-21 20:44:24 +0530

    merge_strings_alternately

--- Two Pointers/merge_strings_alternatively.py
+++ Two Pointers/merge_strings_alternatively.py
@@ -1,3 +1,19 @@
-word1 = "abc", word2 = "pqr"
+word1 = "abc"
+word2 = "pqrt"
 # Expected oputput = apbqcr
 def mergeAlternately(word1: str, word2: str) -> str:
+    w1l,w2l = len(word1), len(word2)
+    res = []
+
+    for i in range(max(w1l,w2l)):
+        if i < w1l:
+            res.append(word1[i])
+        if i < w2l:
+            res.append(word2[i])
+
+    return ''.join(res)
+
+print(mergeAlternately(word1,word2))
+
+# Time = O(n)
+# Space = O(n)
