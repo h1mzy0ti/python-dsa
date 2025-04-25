1dd53a594670f329943d4fffebfc4b6cd51ec6a6
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-04-26 00:35:51 +0530

    added reverse string

--- Hashmap-Hashset/Majority_Element.py
+++ Hashmap-Hashset/Majority_Element.py
@@ -1,19 +1,31 @@
+'''
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+
+'''
+
 nums = [5,5,5,5,5,53,3,32,2]
+
 def majorityElement(nums):
-        elements_frequency = {}
+        elements_frequency = {}                                                 #To store the frequency
 
         for v in nums:
             if v in elements_frequency:
                 elements_frequency[v] += 1
             else:
                 elements_frequency[v] = 1
-
+                                                                            
         majority = 0
-        majorityelement = 0
-        for k, v in elements_frequency.items():
-            if v > majority:
-                majority = v
-                majorityelement = k
-        return majorityelement 
+        majoritykeys = 0
+        for k, v in elements_frequency.items():                                 # To count the maximum value from the dictionary
+            if v > majority:                                                    # If value of the key is greater than majority(initially = 0)
+                majority = v                                                    # update the majority to the current gretest
+                majoritykeys = k                                                # Also update the key that contain the highest frequency
+        return majoritykeys 
 
 print(majorityElement(nums))
