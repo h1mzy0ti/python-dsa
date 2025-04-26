8a9d33c9d2dc87d6ae2e8c87bc2b1032312247c3
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-04-26 21:41:56 +0530

    maximum numbers of balloons added

--- Hashmap-Hashset/MaximumNumberofBalloons.py
+++ Hashmap-Hashset/MaximumNumberofBalloons.py
@@ -7,5 +7,32 @@ You can use each character in text at most once. Return the maximum number of in
 Example 1:
 Input: text = "nlaebolko"
 Output: 1
+Input: text = "loonbalxballpoon"
+Output: 2
 
 '''
+text = "nlaebolko"
+
+def maxNumberOfBallooons(text):
+    freq = {}                                                       # to count the frequency
+    for chars in text:
+        if chars in freq:
+            freq[chars] += 1
+        else:
+            freq[chars] = 1
+
+
+    min_balooon = float('inf')                                      # to compare the minumum since the minimum number of the letter is
+    b = 'balloon'                                                   # the maximun tiem we can construct balloon
+    for char in b:
+        if char == "l" or char == "o":
+            available = freq.get(char,0) // 2                       # Special case since two 'o' or 'l' in one balloon so floor div for int output
+        else:
+            available = freq.get(char,0)
+        min_balooon = min(min_balooon,available)                    # Compare the minimum each time
+
+    return min_balooon
+
+print(maxNumberOfBallooons(text))
+
+
