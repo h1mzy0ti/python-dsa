f89d422d7b1f00b226300cacdd893155474fef3f
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-04-27 21:24:09 +0530

    few changes

--- Monotonic Stack/DailyTemperatures.py
+++ Monotonic Stack/DailyTemperatures.py
@@ -15,14 +15,14 @@ temperatures = [73,74,75,71,69,72,76,73]
 
 
 def dailyTemparatures(t):
-    stack = []
-    result = [0] * len(t)
+    stack = []                                              # Stack to store the indexs
+    result = [0] * len(t)                                   # Main list to store the output by default it is 0
 
     for nums in range(len(t)):
-        while stack and t[nums] > t[stack[-1]]:
-            index = stack.pop()
-            result[index] = nums - index 
-        stack.append(nums)
+        while stack and t[nums] > t[stack[-1]]:             # Condition if the current t[nums] i greater than the t[stack] where stack is 
+            index = stack.pop()                             # storing the index
+            result[index] = nums - index                    # if greater than append to result - in the index of result append nums(index only) - index of the top stack 
+        stack.append(nums)                                  # if stack is empty and t[nums](also cuurent element) is not greater than the top of the stack(index) 
     return result
 
 
