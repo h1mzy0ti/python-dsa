d1a0ba85a35d8c3471b63f65f19e78144c559d87
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-04-30 01:51:34 +0530

    added - minimum common value in teo array

---
+++ Two Pointers/minimum_common-value_in_two_array.py
@@ -0,0 +1,14 @@
+'''
+Minimum Common Value
+
+Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
+
+Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
+
+Example 1:
+
+Input: nums1 = [1,2,3], nums2 = [2,4]
+Output: 2
+Explanation: The smallest element common to both arrays is 2, so we return 2.
+
+'''
