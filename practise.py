008c545f1156688a4ce91c917ddc9848e8d40bc1
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-05-01 00:51:41 +0530

    added - sum of square numbers (two pointers)

--- practise.py
+++ practise.py
@@ -1,18 +1,4 @@
-def sortedSquares(nums):
-    result = []                                             
-    left, right = 0, len(nums) -1
+x = 5
+c = x ** 0.5
 
-    for n in range(len(nums)):                              
-        square  = nums[n] * nums[n]
-        nums[n] = square
-    
-    while left <= right:                                    
-        if nums[left] > nums[right]:
-            result.append(nums[left])
-            left += 1
-        else:
-            result.append(nums[right])
-            right -=1
-
-    result.reverse()                                       
-    return result
+print(c)
