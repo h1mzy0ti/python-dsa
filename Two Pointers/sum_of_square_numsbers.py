f7aca924d4d94f7316e7002e0112d258f9f454ac
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-05-01 00:51:58 +0530

    added - sum of square numbers (two pointers)

--- Two Pointers/sum_of_square_numsbers.py
+++ Two Pointers/sum_of_square_numsbers.py
@@ -15,3 +15,20 @@ Input: c = 3
 Output: false
 
 '''
+c = 3
+def judgeSquareSum(c):
+    left, right = 0, int(c ** 0.5) 
+
+    while left <= right:
+        sum_square = left * left + right * right
+
+        if sum_square == c:
+            return True
+        elif sum_square < c:
+            left += 1
+        else: 
+            righ -= 1
+    return False
+
+  
+print(judgeSquareSum(c))
