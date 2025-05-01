58014bdbd5e62bf0c9888317ce67ee7e75404610
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-05-02 01:54:47 +0530

    added - arranging coins

---
+++ Binary Search/arranging_coins.py
@@ -0,0 +1,25 @@
+'''
+441. Arranging Coins
+
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+________
+| C |   |
+| C | C |
+| C | C |
+---------
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+
+________
+| C |   |
+| C | C |___
+| C | C | C |
+| C | C |   |
+------------
+Input: n = 8
+Output: 3
+
+'''
