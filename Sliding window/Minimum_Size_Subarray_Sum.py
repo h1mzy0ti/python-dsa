ba7b0dc9cf2aab48b8b62990a279f3055d4f475a
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-05-07 01:15:28 +0530

    added detailed comments - minimum size subarray sun (variable size sliding window)

--- Sliding window/Minimum_Size_Subarray_Sum.py
+++ Sliding window/Minimum_Size_Subarray_Sum.py
@@ -13,16 +13,21 @@ Explanation: The subarray [4,3] has the minimal length under the problem constra
 nums = [2,3,1,2,4,3]
 target = 7
 
-def minSubArrayLen (target, nums):
-    n = len(nums)
-    l,r = 0,0
+def minSubArrayLen(nums, target):
+    n = len(nums)                               
+    left = 0
+    current_sum = 0                                                     # To storethe current window sum (will increase in the for loop until we get a value >= target)
+    min_lenght = float('inf')
 
-    
+    for right in range(n):
+        current_sum += nums[right]
 
+        while current_sum >= target:                                    # If we get a value that is greater tha nr equal to target we will measure the lenght
+            min_lenght = min(min_lenght, right - left + 1)              # And we will decrease the size from the size from left because we need minimal lenght
+            current_sum -= nums[left]
+            left += 1                                                   # And while decreasing the length we we find that the current sum is not satisfying our condition we will exit and return to the for loop and increase the size by nums[right]
 
-        
-
-
+    return min_lenght
 
 
 print(minSubArrayLen(nums,target))
