f0a67046036abee6d81e0ae2d508d60d3ab630c0
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-05-07 00:15:17 +0530

    added  - minimum size subarray sun (variable size sliding window)

---
+++ Sliding window/Minimum_Size_Subarray_Sum.py
@@ -0,0 +1,28 @@
+'''
+Minimum Size Subarray Sum
+
+Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+'''
+nums = [2,3,1,2,4,3]
+target = 7
+
+def minSubArrayLen (target, nums):
+    n = len(nums)
+    l,r = 0,0
+
+    
+
+
+        
+
+
+
+
+print(minSubArrayLen(nums,target))
