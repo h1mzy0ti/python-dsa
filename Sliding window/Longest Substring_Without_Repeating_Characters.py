378a03b81f0eb7cdf1912e2b47b9cb85760f6550
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-05-08 01:43:04 +0530

    added - Longest Substring Without Repeating char (variable sized sliding window)

--- Sliding window/Longest Substring_Without_Repeating_Characters.py
+++ Sliding window/Longest Substring_Without_Repeating_Characters.py
@@ -9,7 +9,30 @@ Output: 3
 Explanation: The answer is "abc", with the length of 3.
 
 '''
-s = "abcabcbb"
+s = "pwwkew"
 def lengthOfLongestSubstring(s):
+    n = len(s)
+    res = set()                                                     # set to not store duplicate
+    right = 0                                                       # to  increase the size 
+    left = 0                                                        # to decrease the size 
+    max_sub = 0                                                     # to count the size 
+
+    for right in range(n):
+        while s[right] in res:
+            res.discard(s[left])
+            left += 1
+
+        res.add(s[right])
+        max_sub = max(max_sub,right - left + 1)
+            
+
+    return max_sub
+            
+
+
+
+
+
+
 
 print(lengthOfLongestSubstring(s))
