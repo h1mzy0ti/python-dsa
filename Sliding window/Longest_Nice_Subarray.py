bb7d7743acb3ac1200a34249ab0c820d1c69c3e7
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-05-08 01:56:54 +0530

    added - Longest nice substring (variable sized sliding window)

---
+++ Sliding window/Longest_Nice_Subarray.py
@@ -0,0 +1,19 @@
+'''
+You are given an array nums consisting of positive integers.
+We call a subarray of nums nice if the bitwise AND of every pair of elements that are in different positions in the subarray is equal to 0.
+Return the length of the longest nice subarray.
+A subarray is a contiguous part of an array.
+Note that subarrays of length 1 are always considered nice.
+
+Input: nums = [1,3,8,48,10]
+Output: 3
+Explanation: The longest nice subarray is [3,8,48]. This subarray satisfies the conditions:
+- 3 AND 8 = 0.
+- 3 AND 48 = 0.
+- 8 AND 48 = 0.
+
+'''
+nums = [1,3,8,48,10]
+
+def longestNiceSubarray(nums):
+print(longestNiceSubarray(nums))
