cfc3c03f82bd88638d21824b8a6a9cba748759e1
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-05-07 19:42:11 +0530

    updated 1243.py

--- Sliding window/1343.py
+++ Sliding window/1343.py
@@ -13,25 +13,28 @@ other sub-arrays of size 3 have averages less than 4 (the threshold).
 
 
 '''
-arr = [2,2,2,2,5,5,5,8]
+arr = [11,13,17,23,29,31,7,5,2,3]
+
 k = 3
-threshold = 4
+threshold = 5
 
 def numOfSubarrays(arr,k,threshold):
     n = len(arr)
     subarr = 0
 
-    window_avg = int(sum(arr[:k]) / k)
+    window_sum = sum(arr[:k])
+
+    if window_sum >= threshold * k:                                                 # if the threshold matches count it
+        subarr += 1
+   
   
     for i in range(k,n):
-        if window_avg >= threshold:
+        window_sum = window_sum -  arr[i - k] + arr[i]                              # by default move the window left to right
+        if  window_sum >= threshold * k:                                            # if threshold is found count it 
             subarr +=1
-        else:
-            window_avg += arr[i - k] + arr[i]
-
-    return subarr - 1
 
 
+    return subarr 
 
 
 print(numOfSubarrays(arr,k,threshold))
