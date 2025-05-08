2408c9aaef31e0c825095a07d09305dfe1e72a0a
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-05-09 02:52:40 +0530

    added - Max consecutive ones III

--- Sliding window/Max_Consecutive_Ones_III.py
+++ Sliding window/Max_Consecutive_Ones_III.py
@@ -13,6 +13,31 @@ nums = [1,1,1,0,0,0,1,1,1,1,0]
 k = 2
 
 def longestOnes(nums,k):
+    n = len(nums)
+    left = 0
+    maximum = 0
+    zero_count = 0
+
+    for right in range(n):
+
+        if nums[right] == 0:
+            zero_count += 1
+
+            while zero_count > k:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1   
+
+
+        maximum = max(maximum, right - left + 1)
+
+    return maximum
+
+
+
+
+
+
 
 
 
