ec625fec68174b64612c679396c2a9bf7ace63f7
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-05-09 13:43:01 +0530

    added - comments in Max_coinsecutive_ones lIII

--- Sliding window/Max_Consecutive_Ones_III.py
+++ Sliding window/Max_Consecutive_Ones_III.py
@@ -16,20 +16,20 @@ def longestOnes(nums,k):
     n = len(nums)
     left = 0
     maximum = 0
-    zero_count = 0
+    zero_count = 0                                                        # Rather than inseritng into new array by fliping 0 we can count zero
 
     for right in range(n):
 
-        if nums[right] == 0:
+        if nums[right] == 0:                                               # If the right is zero count that and also chec kthe length
             zero_count += 1
 
-            while zero_count > k:
-                if nums[left] == 0:
-                    zero_count -= 1
-                left += 1   
+            while zero_count > k:                                           # if the zero count is greater than the threshold k
+                if nums[left] == 0:                                         # and if count is maxtha nk but also zero is cokking inthe way
+                    zero_count -= 1                                         # minus zero if left finds zero
+                left += 1                                                   # Increase the left pointer untill its valid will go to the nums[left] == 0
 
 
-        maximum = max(maximum, right - left + 1)
+        maximum = max(maximum, right - left + 1)                            # Also check the length
 
     return maximum
 
