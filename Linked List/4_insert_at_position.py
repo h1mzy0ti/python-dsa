aea1e78b5d04006e7bc5f536297293e670059748
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-05-10 23:12:05 +0530

    added Linked list insert at position

---
+++ Linked List/4_insert_at_position.py
@@ -0,0 +1,7 @@
+'''
+Positon at 0 = Insert at begining 
+in Between = need to traverse
+Positon at end = Similar t append (untill the last node is reached)
+
+'''
+class 
--- practise.py
+++ practise.py
@@ -11,7 +11,11 @@ class LinkedList:
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node 
-        
+
+    def insert_at_position(self, data, position):
+        new_node = Node(data)
+
+
     def traverse(self):
 
         current = self.head
@@ -23,7 +27,9 @@ class LinkedList:
 
 ll = LinkedList()
 
-ll.append(23)
-ll.append(4)
+ll.append(3)
+ll.append(1)
+
+ll.insert_at_position(2,1)
 
 ll.traverse()
